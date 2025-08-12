825145c8c5a5a1a25434df5031c677aa89b4dee7
Jasyd <jasydavid45@gmail.com>
2025-08-11 23:34:56 -0500

    organize into folders

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Equivalent to: g++ -o prog main.cpp -I/usr/local/include -L/usr/local/lib -Wl,-rpath,/usr/local/lib -Wl,--enable-new-dtags -lSDL3
-g++ -o prog main.cpp `pkg-config --cflags --libs sdl3`
-./prog 
+mkdir -p build
+g++ -o ./build/prog ./src/main.cpp `pkg-config --cflags --libs sdl3`
+./build/prog 
