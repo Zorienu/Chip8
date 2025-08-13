840d7d438012a658b36ea94bda5a14fcbf6716cd
Jasyd <jasydavid45@gmail.com>
2025-08-12 23:05:59 -0500

    add core variables for the emulator

--- src/chip8.h
+++ src/chip8.h
@@ -1,8 +1,27 @@
 #include <array>
 
+#define KEYPAD_SIZE 16
+#define REGISTER_COUNT 16
+#define MEMORY_SIZE 4096
+#define STACK_SIZE 16
+#define VIDEO_WIDTH 64
+#define VIDEO_HEIGHT 32
+#define VIDEO_SIZE (VIDEO_WIDTH * VIDEO_HEIGHT)
+
 class Chip8 {
     public:
-        // TODO
+        std::array<uint8_t, VIDEO_SIZE> gfx;
+        std::array<uint8_t, KEYPAD_SIZE> keys;
+
     private:
-        // TODO
+        std::array<uint8_t, MEMORY_SIZE> memory;
+        std::array<uint8_t, REGISTER_COUNT> V;
+        std::array<uint16_t, STACK_SIZE> stack;
+
+        uint16_t opcode;
+        uint16_t sp;
+        uint8_t delay_timer;
+        uint8_t sound_timer;
+        uint16_t I; // Index register
+        uint16_t pc; // Program counter
 };
